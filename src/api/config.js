4ddd2c6e9ec540c4cef4a653dfad007cf82901c1
karim1safan <iibdae.almubtadi@gmail.com>
2026-04-18 15:27:08 +0200

    delete api from import.env

--- src/api/config.js
+++ src/api/config.js
@@ -1,5 +1,2 @@
 const DEFAULT_API_BASE_URL =
   "https://herbal-api-v1-geg9dub2brgee4ag.austriaeast-01.azurewebsites.net";
-
-export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL?.trim() || DEFAULT_API_BASE_URL;
